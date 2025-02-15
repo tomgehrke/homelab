48a09218230d1b55d76543a0da0a82b229997844
Tom Gehrke <tomgehrke@gmail.com>
2025-02-15 15:52:47 -0600

    Add check for root

--- scripts/add-sudoer.sh
+++ scripts/add-sudoer.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ $EUID == 0 ]]; then
+        echo "User is already root!"
+        exit 1
+fi
+
 if [[ ! -f /etc/sudoers.d/$USER ]]; then
     echo "Adding $USER as sudoer..."
     echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
