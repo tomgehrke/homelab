586a8f198130a8069fe9dd37fcf000b9cc7cbdbd
Tom Gehrke <tomgehrke@gmail.com>
2025-04-26 22:47:35 -0500

    Add script to install postgresql

---
+++ scripts/bash/system/install-postgresql.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+. /etc/os-release
+sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+sudo apt update
+sudo apt -y install postgresql
