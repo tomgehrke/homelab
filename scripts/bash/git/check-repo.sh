6bb1fe68aa5e07daf24fe9bf1313740f42caaf18
Tom Gehrke <tomgehrke@gmail.com>
2025-06-07 11:35:31 -0500

    Repo check enhancements

--- scripts/bash/git/check-repo.sh
+++ scripts/bash/git/check-repo.sh
@@ -2,29 +2,55 @@
 
 checkRepo() {
         local repoPath="$1"
+        local fetchResult
+        local localUnstaged localUncommitted localBehind localAhead localUntracked
 
-        git --quiet -C "$repoPath" fetch origin
-
-        # See if local repo has unstaged changes...
-        if ! git -C "$repoPath" diff; then
-                echo -e "Your Homelab repo has unstaged changes.\n"
+        git -C "$repoPath" fetch --quiet
+        fetchResult=$?
+        if [[ $fetchResult -gt 0 ]]; then
+                exit $fetchResult
         fi
 
-        # See if local repo has uncommitted changes...
-        if ! git -C "$repoPath" diff --cached; then
-                echo -e "Your Homelab repo has staged but uncommited changes.\n"
+        # See if local repo has unstaged changes
+        git -C "$repoPath" diff --quiet || localUnstaged=true
+
+        # See if local repo has uncommitted changes
+        git -C "$repoPath" diff --cached --quiet || localUncommitted=true
+
+        # See if local repo is behind
+        localBehind=$(git -C "$repoPath" rev-list --quiet --count @..@{u})
+        [[ "$localBehind" -gt 0 ]] && localBehind=true
+
+        # See if local repo is ahead
+        localAhead=$(git -C "$repoPath" rev-list --quiet --count @{u}..@)
+        [[ "$localAhead" -gt 0 ]] && localAhead=true
+
+        # Check for untracked files
+        [[ -n "$(git -C "$repoPath" ls-files --others --exclude-standard)" ]] && localUntracked=true
+
+        # Report Results
+        if [[ "$localUnstaged" == true ]]; then
+                echo "==> ${repoPath} has unstaged changes."
         fi
 
-        # See if local repo is behind...
-        if (( $(git -C "$repoPath" rev-list --count @..@{u} ) > 0 )); then
-                if read -p "Homelab repo needs updating. Would you like to do this now? (y/n): " response && [[ "${response,,}" == "y" ]]; then
+        if [[ "$localUncommitted" == true ]]; then
+                echo "==> ${repoPath} has staged but uncommitted changes."
+        fi
+
+        if [[ "$localUntracked" == true ]]; then
+                echo "==> ${repoPath} has untracked files."
+        fi
+
+        if [[ "$localBehind" == true ]]; then
+                read -p "==> ${repoPath} needs updating. Would you like to do this now? (y/n): " response
+                if [[ "${response,,}" == "y" ]]; then
                         git -C "$repoPath" pull
                 fi
         fi
 
-        # See if local repo is ahead...
-        if (( $(git -C "$repoPath" rev-list --count @{u}..@ ) > 0 )); then
-                if read -p "You have local Homelab changes. Push to remote? (y/n): " response && [[ "${response,,}" == "y" ]]; then
+        if [[ "$localAhead" == true ]]; then
+                read -p "==> ${repoPath} has local changes. Push to remote? (y/n): " response
+                if [[ "${response,,}" == "y" ]]; then
                         git -C "$repoPath" push
                 fi
         fi
