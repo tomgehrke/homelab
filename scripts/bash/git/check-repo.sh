da505fc43d60fbc6212a021e6013c7460562b136
Tom Gehrke <tomgehrke@gmail.com>
2025-06-28 22:32:31 -0500

    Include .bashrc.d support and add git repository checks

--- scripts/bash/git/check-repo.sh
+++ scripts/bash/git/check-repo.sh
@@ -3,8 +3,7 @@
 checkRepo() {
         local repoPath="$1"
         local fetchResult
-        local localUnstaged localUncommitted localBehind localAhead localUntracked
-        local attentionRequired
+        local localStatus behindCount localBehind aheadCount localAhead
 
         git -C "$repoPath" fetch --quiet
         fetchResult=$?
@@ -13,60 +12,44 @@ checkRepo() {
         fi
 
         # See if local repo has unstaged changes
-        git -C "$repoPath" diff --quiet || localUnstaged=true
+        git -C "$repoPath" diff --quiet || localStatus="${localStatus}, UNSTAGED"
 
         # See if local repo has uncommitted changes
-        git -C "$repoPath" diff --cached --quiet || localUncommitted=true
-
-        # See if local repo is behind
-        localBehind=$(git -C "$repoPath" rev-list --quiet --count @..@{u})
-        [[ "$localBehind" -gt 0 ]] && localBehind=true
-
-        # See if local repo is ahead
-        localAhead=$(git -C "$repoPath" rev-list --quiet --count @{u}..@)
-        [[ "$localAhead" -gt 0 ]] && localAhead=true
+        git -C "$repoPath" diff --cached --quiet || localStatus="${localStatus}, UNCOMMITTED"
 
         # Check for untracked files
-        [[ -n "$(git -C "$repoPath" ls-files --others --exclude-standard)" ]] && localUntracked=true
+        [[ -n "$(git -C "$repoPath" ls-files --others --exclude-standard)" ]] && localStatus="${localStatus}, UNTRACKED"
+
+        # See if local repo is behind
+        behindCount=$(git -C "$repoPath" rev-list --quiet --count @..@{u})
+        [[ "$behindCount" -gt 0 ]] && localBehind=true
+
+        # See if local repo is ahead
+        aheadCount=$(git -C "$repoPath" rev-list --quiet --count @{u}..@)
+        [[ "$aheadCount" -gt 0 ]] && localAhead=true
 
         # Report Results
-        if [[ "$localUnstaged" == true ]]; then
-                attentionRequired=true
-                echo "==> ${repoPath} has unstaged changes."
+
+        if [[ -n "$localStatus" ]]; then
+                echo "=> ${repoPath} status: ${localStatus:2}"
+                echo
         fi
 
-        if [[ "$localUncommitted" == true ]]; then
-                attentionRequired=true
-                echo "==> ${repoPath} has staged but uncommitted changes."
-        fi
-
-        if [[ "$localUntracked" == true ]]; then
-                attentionRequired=true
-                echo "==> ${repoPath} has untracked files."
-        fi
-
-        if [[ "$localBehind" == true ]]; then
-                attentionRequired=true
-                read -p "==> ${repoPath} needs updating. Would you like to do this now? (y/n): " response
-                if [[ "${response,,}" == "y" ]]; then
+        if [[ "$localBehind" = true ]]; then
+                read -p "=> ${repoPath} needs updating. Would you like to do this now? (y/n): " response
+                if [[ "${response,,}" = "y" ]]; then
                         git -C "$repoPath" pull
                 fi
         fi
 
-        if [[ "$localAhead" == true ]]; then
-                attentionRequired=true
-                read -p "==> ${repoPath} has local changes. Push to remote? (y/n): " response
-                if [[ "${response,,}" == "y" ]]; then
+        if [[ "$localAhead" = true ]]; then
+                read -p "=> ${repoPath} has local changes. Push to remote? (y/n): " response
+                if [[ "${response,,}" = "y" ]]; then
                         git -C "$repoPath" push
                 fi
         fi
-
-        if [[ "$attentionRequired" == true ]]; then
-                echo
-        fi
-
 }
 
-if [[ $- == *i* && "${BASH_SOURCE[0]}" == "$0" ]]; then
+if [[ $- = *i* && "${BASH_SOURCE[0]}" = "$0" ]]; then
         checkRepo "$1"
 fi
